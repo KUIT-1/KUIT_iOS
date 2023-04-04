7e8aa0e2deec457c7e0e285530f32e039fcdbf2e
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-04 20:52:10 +0900

    Week3_이안진_Modal 화면 전환 방식

--- anjin/Week3/Week3/ViewController.swift
+++ anjin/Week3/Week3/ViewController.swift
@@ -14,8 +14,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func Button2Tap(_ sender: Any) {
-        self.performSegue(withIdentifier: "Segue1", sender: self)
+    @IBAction func button1Tap(_ sender: Any) {
+        // modal.presentation
+        let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "VC2")
+        nextVC?.modalPresentationStyle = .fullScreen
+        nextVC?.modalTransitionStyle = .flipHorizontal
+        self.present(nextVC! , animated: true)
+    }
+    
+    @IBAction func button2Tap(_ sender: Any) {
+        // modal.presentation
+        let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "VC2")
+        nextVC?.modalPresentationStyle = .fullScreen
+        // 종이가 넘어가는 것처럼
+        nextVC?.modalTransitionStyle = .partialCurl
+        self.present(nextVC! , animated: true)
     }
     
 }
