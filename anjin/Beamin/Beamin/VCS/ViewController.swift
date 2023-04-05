3be9d277a8fdfa3435be42b5f109ade6f8eee4a2
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-05 15:35:56 +0900

    Week3_이안진_배달 Btn

--- anjin/Beamin/Beamin/VCS/ViewController.swift
+++ anjin/Beamin/Beamin/VCS/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var orderBG: UIImageView!
+    @IBOutlet weak var orderView: UIView!
+    @IBOutlet weak var orderLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        orderButtonInit()
     }
-
+    
+    @IBAction func orederButtonTap(_ sender: Any) {
+        // 배달 화면
+    }
+    
+    func orderButtonInit() {
+        orderLable.text = "세상은 넓고\n맛집은 많다"
+        orderBG.layer.cornerRadius = 10
+        orderView.layer.cornerRadius = 10
+        
+        orderView.layer.shadowColor = UIColor.black.cgColor
+        orderView.layer.shadowOpacity = 0.1
+        orderView.layer.shadowRadius = 10
+    }
 
 }
 
