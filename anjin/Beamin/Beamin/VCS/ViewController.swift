655fed6f1cdf624c12e91675c59799e27dd66c35
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-04-05 15:52:03 +0900

    Week3_이안진_배민1 Btn

--- anjin/Beamin/Beamin/VCS/ViewController.swift
+++ anjin/Beamin/Beamin/VCS/ViewController.swift
@@ -8,20 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var orderBG: UIImageView!
+    @IBOutlet weak var orderOneView: UIView!
+    @IBOutlet weak var orderOneBG: UIImageView!
+    @IBOutlet weak var orderOneLabel: UILabel!
+    
     @IBOutlet weak var orderView: UIView!
+    @IBOutlet weak var orderBG: UIImageView!
     @IBOutlet weak var orderLable: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        orderOneButtonInit()
         orderButtonInit()
+    }
+    
+    @IBAction func orderOneButtonTap(_ sender: Any) {
+        // 배민1 화면
     }
     
     @IBAction func orederButtonTap(_ sender: Any) {
         // 배달 화면
+    }
+    
+    
+    func orderOneButtonInit() {
+        orderOneLabel.text = "야구선수 뽑으면\n최대 1만원 쿠폰"
+        orderOneBG.layer.cornerRadius = 10
+        orderOneView.layer.cornerRadius = 10
+        
+        orderOneView.layer.shadowColor = UIColor.black.cgColor
+        orderOneView.layer.shadowOpacity = 0.1
+        orderOneView.layer.shadowRadius = 10
     }
     
     func orderButtonInit() {
