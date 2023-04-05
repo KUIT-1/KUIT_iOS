0445f612b4da411ca5fee5027b796fe442b6ce63
slr-09 <ge12090@naver.com>
2023-04-05 19:39:02 +0900

    week3_유가은

---
+++ slr-09/baemin_clone/baemin_clone/foodButton.swift
@@ -0,0 +1,38 @@
+//
+//  food.swift
+//  baemin_clone
+//
+//  Created by 가은 on 2023/04/05.
+//
+
+import UIKit
+
+class foodButton: UIView {
+
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var foodLabel: UILabel!
+    @IBOutlet weak var foodBT: UIButton!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        customInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    func customInit(){
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+        
+    }
+    
+    func loadViewFromNib() -> UIView?{
+        let nib = UINib(nibName: "foodButton", bundle: nil)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
