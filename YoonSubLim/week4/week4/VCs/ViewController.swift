4662af1b3b3d081af618b88617a582523ba436f3
YoonSubLim <yslim37@naver.com>
2023-04-26 17:36:08 +0900

    Week4_임윤섭_guard 구문 사용하여 ViewController 객체 만들기

--- YoonSubLim/week4/week4/VCs/ViewController.swift
+++ YoonSubLim/week4/week4/VCs/ViewController.swift
@@ -36,12 +36,22 @@ class ViewController: UIViewController {
         print("로그인 버튼 클릭")
         
         // 로그인 스토리보드 찾기
-        let loginSB = UIStoryboard(name: "Login", bundle: nil)
-        print("@@@1")
-        let loginVC = loginSB.instantiateViewController(withIdentifier: "login")
-        print("@@@2")
-
-        self.navigationController?.pushViewController(loginVC, animated: true)
+        // guard let 으로 감싸려 했으나, UIStoryboard 는 Optional 값이 아니고, 못 찾을 경우에는 nil이 아닌에러를 던짐
+        do {
+            print("@@@1")
+            let loginSB = try UIStoryboard(name: "Login", bundle: nil)
+            
+            print("@@@2")
+            let loginVC = loginSB.instantiateViewController(withIdentifier: "login")
+            self.navigationController?.pushViewController(loginVC, animated: true)
+            
+        } catch {
+            // handle the error here
+            print("Unable to initialize storyboard: \(error.localizedDescription)")
+        }
+        
+//        guard let loginVC2 = self.storyboard?.instantiateViewController(withIdentifier: "test") else { return }
+        
         
     }
 
