dbfad64b70866ae691e955d2e478eb8d2b1838cb
slr-09 <ge12090@naver.com>
2023-04-27 16:06:41 +0900

    week4 login 화면

--- slr-09/baemin_clone/baemin_clone/AppDelegate.swift
+++ slr-09/baemin_clone/baemin_clone/AppDelegate.swift
@@ -11,20 +11,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    // 구현중인 앱이 처음 실행되면서 앱의 셋업이 완료되었을때만 호출
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
+    // 사용자나 시스템이 새로운 Scene을 생성했을때 호출
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    // 유저가 app switcher를 통하여 Scene을 닫을 때 호출
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
