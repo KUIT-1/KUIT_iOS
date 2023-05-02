b3c990d1fbef1b1c6d0633f79eaa34c63b0c3777
YoonSubLim <yslim37@naver.com>
2023-05-02 12:36:29 +0900

    Week5_임윤섭_protocol/extension 실습

--- YoonSubLim/week5/week5/ViewController.swift
+++ YoonSubLim/week5/week5/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, protocol_Person {
+    
+    var first_name: String  = "김" // first_name 은 get 이므로, var / let 모두 선언 가능하다.
+//    let first_name: String  = ""
+    
+    
+    var last_name: String = "땡땡"
+    
+//    func get_fullName() {
+//        print(self.first_name + self.last_name + " 입니다.")
+//    } // -> extension 으로 대체 // 필요시 여기서 재정의해도 된다. 우선순위가 높아서 대체됨.
 
+    var test1: Int = 1 // var : 가변 // 읽기, 쓰기
+    let test2: Int = 2 // let : 불변 // 읽기
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        test1 = 2
+//        test2 = 1 // 불가
+        
+        print(test1.isOdd)
+        get_fullName()
+        
     }
 
 
 }
 
+// 사람
+// 성, 이름 -> 프로퍼티
+// 전체이름 -> 함수
+
+protocol protocol_Person{
+    // var 변수명: 변수타입 { get set }
+    // 변수 선언에는 var, let 등이 있지만, 프로토콜에서는 var 를 사용해야 한다.
+    var first_name: String { get } // 읽을 수 있다. -> 구현 시 var, let 가능
+    var last_name: String { get set } // 읽고 쓸 수 있다. -> 구현시 let 만 가능
+    
+    func get_fullName()
+}
+
+extension Int{ // 이름 X. 정수에 대한 확장이다.
+    var isOdd:Bool{
+        return self % 2 == 1
+    }
+    
+}
+
+// 위의 경우와 마찬가지로, 프로토콜에 대한 extension 을 만들어, 기본적인 함수 내용을 구현하자.
+extension protocol_Person{
+    
+    // 연산 프로퍼티
+    var first_name: String{
+        return "first"
+    }
+    
+    func get_fullName(){
+        print(self.first_name + self.last_name)
+    }
+}
