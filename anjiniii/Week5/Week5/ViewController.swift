bd53d740d7162479b28c5401b17e013550dd7390
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-03 16:03:23 +0900

    Week5_이안진_textfield Viewcontroller

--- anjiniii/Week5/Week5/ViewController.swift
+++ anjiniii/Week5/Week5/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController, Person {
+class ViewController: UIViewController, Person, DataDelegate {
+    
+    @IBOutlet weak var label: UILabel!
+    func sendData(textData: String) {
+        label.text = textData
+    }
+    @IBAction func button(_ sender: Any) {
+        let VC1 = self.storyboard?.instantiateViewController(withIdentifier: "VC2") as! ViewController1
+        VC1.delegate = self
+        
+        self.navigationController?.pushViewController(VC1, animated: true)
+    }
+    
     
     var firstName: String = ""
     var lastName: String = ""
     func getName() {
         print("name: \(self.firstName)\(self.lastName)")
     }
+    
     
     var test1: Int = 1
     let test2: Int = 2
