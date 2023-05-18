3e52d210fad8138a4762013ec88ae5def2a59245
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-18 22:45:57 +0900

    Week6_이안진_UIColor를 보여주는 CollectionView 구현

--- anjiniii/Week6/Week6/ViewController.swift
+++ anjiniii/Week6/Week6/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var testCollectionView: UICollectionView!
+    let colorTable: [UIColor] = [UIColor.black , UIColor.red, UIColor.blue , UIColor.green , UIColor.gray, UIColor.magenta]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        testCollectionView.dataSource = self
+        testCollectionView.delegate = self
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // 몇 개의 셀이 있는지
+        let colorTable: [UIColor] = [UIColor.black, UIColor.red, UIColor.blue, UIColor.green, UIColor.gray, UIColor.magenta]
+        return colorTable.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // 어떤 셀을 사용할건지
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "testCell", for: indexPath) as! testCollectionViewCell
+        
+        let colorTable: [UIColor] = [UIColor.black , UIColor.red, UIColor.blue , UIColor.green , UIColor.gray, UIColor.magenta]
+        cell.myView.backgroundColor = colorTable[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 각 셀의 크기가 어떻게 되는지
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+}
---
+++ anjiniii/Week6/Week6/testCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  testCollectionViewCell.swift
+//  Week6
+//
+//  Created by 이안진 on 2023/05/18.
+//
+
+import UIKit
+
+class testCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myView: UIView!
+    
+}
