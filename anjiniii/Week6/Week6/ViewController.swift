96f28e63f227169f9cadabfb01a9a68c7bbf07eb
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-18 21:20:51 +0900

    Week6_이안진_first commit

---
+++ anjiniii/Week6/Week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week6
+//
+//  Created by 이안진 on 2023/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
