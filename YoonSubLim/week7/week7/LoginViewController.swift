83efcff672f02e6ffa9f32aa0369e24135908a49
YoonSubLim <yslim37@naver.com>
2023-05-23 02:33:43 +0900

    Week7_임윤섭_Login Storyboard & Asset 추가

---
+++ YoonSubLim/week7/week7/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController1.swift
+//  week7
+//
+//  Created by YoonSub Lim on 2023/05/23.
+//
+
+import UIKit
+
+// Login Storyboard 파일을 copy & add 하고,
+// Asset 을 추가해준 후, 기존 connection 을 다 끊어주었다.
+// 그리고 로그인 관련 코드만 보여주기 위해,
+// first storyboard 설정을 info.plist 에서 Main -> Login 로 변경
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
