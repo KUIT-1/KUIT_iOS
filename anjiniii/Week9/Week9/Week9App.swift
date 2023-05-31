d0c1703f9125f349039cf1a2e157beb4905e2bf1
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-05-31 15:38:08 +0900

    Week9_이안진_first commit

---
+++ anjiniii/Week9/Week9/Week9App.swift
@@ -0,0 +1,17 @@
+//
+//  Week9App.swift
+//  Week9
+//
+//  Created by 이안진 on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct Week9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
